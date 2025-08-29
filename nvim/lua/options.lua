ab2a0f960f223d10c26c62e68bd4bb613ec1912c
Michael Froeber <michael.froeber@gmail.com>
2025-08-29 10:45:13 +0200

    added nvim

---
+++ nvim/lua/options.lua
@@ -0,0 +1,13 @@
+require "nvchad.options"
+
+-- add yours here!
+
+-- local o = vim.o
+-- o.cursorlineopt ='both' -- to enable cursorline!
+local M = {}
+
+M.options = {
+  clipboard = "unnamedplus", -- use system clipboard
+}
+
+return M
